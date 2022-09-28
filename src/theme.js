4775dc22f84e61e15c57f7afbbbfbdecdb8137d6
edut23 <edut23@gmail.com>
2022-09-27 23:35:43 -0300

    Portfolio

---
+++ src/theme.js
@@ -0,0 +1,39 @@
+import BGLight from './todo-app-main/images/bg-desktop-light.jpg'
+import BGDark from './todo-app-main/images/bg-desktop-dark.jpg'
+
+
+export const lightTheme = {
+    CreateBar: 'hsl(0, 0%, 98%)',
+    Todos: 'hsl(0, 0%, 98%)',
+    TextTodos: 'hsl(235, 19%, 35%)',
+    Input: 'hsl(0, 0%, 98%)',
+    BodyColor: 'hsl(236, 33%, 92%)',
+    LightGrayishBlue: 'hsl(233, 11%, 84%)',
+    TextCreateBar: 'hsl(236, 9%, 61%)',
+    ItemBorder: 'hsl(236, 9%, 61%)',
+    TextInput: 'hsl(235, 19%, 35%)',
+    TextFinal: 'hsl(236,9%,61%)',
+    Filter: 'hsl(234, 39%, 85%)',
+    BackgroundImage: BGLight
+}
+
+export const darkTheme = {
+    VeryDarkBlue: 'hsl(235, 21%, 11%)',
+    VeryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+    Filter: 'hsl(234, 39%, 85%)',
+    LightGrayishBlueHover: 'hsl(236, 33%, 92%)',
+    DarkGrayishBlue: 'hsl(234, 11%, 52%)',
+    VeryDarkGrayishBlue: 'hsl(233, 14%, 35%)',
+    VeryDarkGrayishBlueHover: 'hsl(237, 14%, 26%)',
+    CreateBar: 'hsl(233, 14%, 35%)',
+    Todos: 'hsl(233, 14%, 35%)',
+    TextTodos: 'hsl(236, 33%, 92%)',
+    Input: 'hsl(233, 14%, 35%)',
+    BodyColor: 'hsl(235, 21%, 11%)',
+    LightGrayishBlue: 'hsl(233, 11%, 84%)',
+    TextCreateBar: 'hsl(236, 9%, 61%)',
+    ItemBorder: 'hsl(236, 9%, 61%)',
+    TextInput: 'hsl(235, 19%, 35%)',
+    TextFinal: 'hsl(236,9%,61%)',
+    BackgroundImage: BGDark
+}
