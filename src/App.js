b39ea588925e6cd0921775b9b0b491f1669d3301
edut23 <edut23@gmail.com>
2022-09-27 23:36:55 -0300

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, CreateBar, Header, Body, Todos, Input, Item, FinalItem, MainContainer, ThemeIcon, DeleteIcon, CheckIcon, HeaderContent, TextTodo } from './style.js';
+import { ReactComponent as Moon} from './todo-app-main/images/icon-moon.svg';
+import { ReactComponent as Sun} from './todo-app-main/images/icon-sun.svg';
+import { ReactComponent as Delete} from './todo-app-main/images/icon-cross.svg';
+import { ReactComponent as Check} from './todo-app-main/images/Check.svg';
+import { ReactComponent as Circle} from './todo-app-main/images/Circle.svg';
+import {useState, useEffect, useCallback} from 'react';
+import { lightTheme, darkTheme } from './theme';
+
 
 function App() {
+
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [id, setId] = useState(-1);
+  const [items, setItems] = useState(0);
+  const [loaded, setLoaded] = useState(true);
+  const [theme, setTheme] = useState('light');
+  const [check, setCheck] = useState('false');
+  const [filter, setFilter] = useState('all');
+
+  const checkToggler = () => {
+    check === 'false' ? setCheck('true') : setCheck('false')
+  }
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+}
+
+  const data = JSON.parse(localStorage.getItem("Todo"));
+
+  const loader = useCallback(() => {
+    if (data !== null) {
+    if ((data.length > 0) && loaded) {
+      setTodo(data);
+      setItems(data.length);
+      setId(data.length);
+      setLoaded(false);
+      console.log(data);
+    }}
+    else 
+      setLoaded(false);
+    ;}, [setTodo, setId, setItems, data, loaded, setLoaded])
+
+  useEffect(() => {
+    loader();
+    setItems(todo.length);
+    if (todo !== [])
+      localStorage.setItem("Todo", JSON.stringify(todo));
+  }, [setItems, todo])
+
+
+  const textHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  const deleteTodo = useCallback((n) => {
+    const filterTodo = todo.filter((t) => t.id !== n);
+    setTodo(filterTodo);
+  }, [todo, setTodo]);
+
+  const deleteChecked = useCallback(() => {
+    const filterTodo = todo.filter((t) => t.Check !== 'true');
+    setTodo(filterTodo);
+  }, [todo, setTodo]);
+
+  const taskCheckToggler = (n) => {
+    const tempTodo = [...todo];
+    tempTodo[n].Check === 'false' ? tempTodo[n].Check = 'true' : tempTodo[n].Check ='false';  
+    console.log(tempTodo[n].Check);
+    setTodo(tempTodo);
+    console.log(todo);
+  }
+
+
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if(id === -1){
+        setTodo([{id: Math.random(), Note: text, Check: check}]);
+        setId(0);
+        setItems(todo.length);
+      } else {
+      setId(todo.length + 1);
+      setTodo([...todo,{id: Math.random(), Note: text, Check: check}]);
+      setText("");
+      console.log(todo)
+    }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header theme={theme === 'light' ? lightTheme : darkTheme}>
+        <HeaderContent>
+        <h1 className="title">
+          T O D O
+        </h1>
+        <ThemeIcon onClick={themeToggler}>{theme === 'light' ? <Moon/> : <Sun/>}</ThemeIcon>
+        </HeaderContent>
+      </Header>
+      <Body theme={theme === 'light' ? lightTheme : darkTheme}>
+        <MainContainer>
+        <CreateBar theme={theme === 'light' ? lightTheme : darkTheme}>
+        <CheckIcon onClick={checkToggler}>{check === 'true' ? <Check/> : <Circle/>}</CheckIcon>
+            <Input placeholder="Create a new todo..." value={text} theme={theme === 'light' ? lightTheme : darkTheme} onChange={textHandler} onKeyDown={handleKeyDown}/>
+        </CreateBar>
+        <Todos theme={theme === 'light' ? lightTheme : darkTheme}>
+        {id < 0 ? (<FinalItem><p>Press Enter to add a todo</p></FinalItem>) : (
+        todo.map((note, index) => ( filter === 'all' ?
+      <Item
+        theme={theme === 'light' ? lightTheme : darkTheme}
+        key={index}
+      ><CheckIcon onClick={() => taskCheckToggler(index)}>{note.Check === 'true' ? <Check/> : <Circle/>}</CheckIcon><TextTodo>{note.Note}</TextTodo><DeleteIcon><Delete onClick={(e) => deleteTodo(note.id)}/></DeleteIcon></Item>
+      : filter === 'active' ? (note.Check === 'false' &&
+      <Item
+        theme={theme === 'light' ? lightTheme : darkTheme}
+        key={index}
+      ><CheckIcon onClick={() => taskCheckToggler(index)}>{note.Check === 'true' ? <Check/> : <Circle/>}</CheckIcon><TextTodo>{note.Note}</TextTodo><DeleteIcon><Delete onClick={(e) => deleteTodo(note.id)}/></DeleteIcon></Item>)
+      : (note.Check === 'true' &&
+      <Item
+        theme={theme === 'light' ? lightTheme : darkTheme}
+        key={index}
+      ><CheckIcon onClick={() => taskCheckToggler(index)}>{note.Check === 'true' ? <Check/> : <Circle/>}</CheckIcon><TextTodo>{note.Note}</TextTodo><DeleteIcon><Delete onClick={(e) => deleteTodo(note.id)}/></DeleteIcon></Item>)
+      )))}
+        {id >= 0 && <FinalItem theme={theme === 'light' ? lightTheme : darkTheme}><p>{items} items left</p>    {filter === 'all'  ? (<div>  <p1 onClick={(e) => setFilter('all')} style={{color:"hsl(220, 98%, 61%)"}}>All</p1><p1 onClick={(e) => setFilter('active')}>Active</p1><p1 onClick={(e) => setFilter('completed')}>Completed</p1></div>) : filter === 'active'  ? (<div><p1 onClick={(e) => setFilter('all')}>All</p1><p1 onClick={(e) => setFilter('active')} style={{color:"hsl(220, 98%, 61%)"}}>Active</p1><p1 onClick={(e) => setFilter('completed')}>Completed</p1></div>) : (<div><p1 onClick={(e) => setFilter('all')}>All</p1><p1 onClick={(e) => setFilter('active')}>Active</p1><p1 onClick={(e) => setFilter('completed')} style={{color:"hsl(220, 98%, 61%)"}}>Completed</p1></div>)}    <p1 onClick={(e) => deleteChecked()}>Clear Completed</p1></FinalItem>}  
+        </Todos>
+        </MainContainer>
+      </Body>
+    </Container>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,146 @@
+import styled, { css } from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+  font-family: "Josefin Sans", sans-serif;
+`
+
+
+const CreateBar = styled.div`
+  width: 34vw;
+  background-color: ${({theme}) => theme.CreateBar};
+  color: ${({theme}) => theme.TextCreateBar};
+  border-radius: 0.3em;
+  display: flex;
+  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.1);
+  @media (max-width: 500px){
+    width: 84vw;
+  }
+`
+const Input = styled.input`
+  margin: 1em 0em;
+  border: none;
+  background-color: ${({theme}) => theme.Input};
+  outline: none;
+  width: 90%;
+  &:active {
+    border: none;
+    outline: none;
+    color: ${({theme}) => theme.TextInput};
+  }
+`
+const Header = styled.div`
+  background-image: url(${({theme}) => theme.BackgroundImage});
+  background-repeat: no-repeat;
+  background-size: auto; 
+  min-height: 40vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  h1{
+    margin-right: 16.5vw;
+  }
+  @media (max-width: 500px){
+    h1{
+      margin-right: 45vw;
+    }
+  }
+  @media (min-height: 750px){
+    min-height: 35vh;
+  }
+  @media (min-height: 850px){
+    min-height: 30vh;
+  }
+`
+
+const Body = styled.div`
+  background-color: ${({theme}) => theme.BodyColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`
+const MainContainer = styled.div`
+  position: sticky;
+  margin-top: -15vh;
+  margin-bottom: auto;
+`
+const HeaderContent = styled.div`
+  margin-bottom: 5vh;
+  flex-direction: row;
+  display: flex;
+  @media (max-width: 500px){
+    margin-bottom: 10vh;
+  }
+`
+
+const Todos = styled.div`
+  width: 34vw;
+  background-color: ${({theme}) => theme.Todos};
+  color: ${({theme}) => theme.TextTodos};
+  border-radius: 0.4em;
+  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.1);
+  padding: 0.001em 0em;
+  margin-top: 1em;
+  @media (max-width: 500px){
+    width: 84vw;
+  }
+`
+const Item = styled.div`
+  border-width: 0em 0em 0.1em 0em;
+  border-color: ${({theme}) => theme.ItemBorder};
+  border-style: solid;
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+  p {
+    max-width: 20vw;
+    margin: 1em 0 1em 0;
+  }
+`
+const TextTodo = styled.div`
+  max-width: 20vw;
+  margin: 1em 0 1em 0;
+  color: ${({theme}) => theme.TextTodos};
+`
+const FinalItem = styled(Item)`
+  justify-content: space-around;
+  border-width: 0;
+  font-size: 16px;
+  color: ${({theme}) => theme.TextFinal};
+  div{
+    justify-content: space-around;
+    p1{
+      margin: 0 2%;
+    }
+  }
+  p1:hover {
+    color: ${({theme}) => theme.TextTodos};
+  }
+`
+const ThemeIcon = styled.div`
+  width: 5vw;
+  height: 5vh;
+  margin: auto;
+`
+const CheckIcon = styled.div`
+  width: 4vw;
+  height: 2.5vh;
+  margin-bottom: auto;
+  svg {
+    margin: 0.7em;
+  }
+  svg:hover {
+    color: hsl(220, 98%, 61%);
+  }
+`
+const DeleteIcon = styled.div`
+  margin-left: auto;
+  margin-right: 1em;
+`
+
+export { Container, CreateBar, Header, Body, Todos, Input, Item, FinalItem, MainContainer, ThemeIcon, DeleteIcon, CheckIcon, TextTodo, HeaderContent }
